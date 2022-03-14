9b69ecad5dcb8798f5fbb80b459d23351dbc2a6d
Daniel Bourke <danielbourke@icloud.com>
2022-03-14 15:33:32 +1000

    update get data to fetch all rows to begin with

--- get_data.js
+++ get_data.js
@@ -20,47 +20,69 @@ selectElement.addEventListener("change", (event) => {
     food_selection = event.target.value.toLowerCase();
     getFoodData();
     console.log(`Selected food name: ${food_selection}`)
-    console.log(`Target food name: ${fdc_ids_string_index[food_selection]}`)
+    console.log(`Target food ID: ${fdc_ids_string_index[food_selection]}`)
 })
 
-// Get data from Supabase
-async function get_food_data_from_supabase(id) {
+// Get all data in one hit from Supabase (this is all rows in the DB)
+async function get_all_food_data_from_supabase() {
     let { data, error } = await supabase
         .from("food_data_central_nutrition_data_test")
         .select("*")
-        .eq("fdc_id", id)
 
     if (error) {
         console.log(error);
         return;
     }
-    console.log("logging data");
-    console.log("data[0]")
-    return data[0];
+    console.log(data);
+    return data;
 }
+
+// Get all food data in one hit from Supabase and save it to a constant
+const data = await get_all_food_data_from_supabase();
+
+// // Get single row of food data from Supabase (this is a single food)
+// async function get_food_data_from_supabase(id) {
+//     var startTime = performance.now()
+//     let { data, error } = await supabase
+//         .from("food_data_central_nutrition_data_test")
+//         .select("*")
+//         .eq("fdc_id", id)
+
+//     if (error) {
+//         console.log(error);
+//         return;
+//     }
+//     console.log("logging data");
+//     console.log("data[0]")
+//     var endTime = performance.now()
+//     console.log(`Query time: ${(endTime - startTime) / 1000} seconds`)
+//     return data[0];
+
+// }
 
 // Make a function to get food data and then update the DOM elements
 async function getFoodData() {
     var startTime = performance.now()
 
     const target_food_code = fdc_ids_string_index[food_selection];
-    const data = await get_food_data_from_supabase(target_food_code);
+    const target_food_data = data.find(element => element["fdc_id"] == target_food_code)
+    // const data = await get_food_data_from_supabase(target_food_code);
     // You can get data from "data" using JavaScript destructuring
     console.log(data);
-    const food_name = data["food"];
+    const food_name = target_food_data["food"];
     console.log(food_name);
     document.getElementById("food_name").textContent = food_name;
 
     // Get nutrient values
-    const protein_amount = data["protein"];
+    const protein_amount = target_food_data["protein"];
     console.log(`Protein amount: ${protein_amount}`)
     document.getElementById("protein_amount").textContent = protein_amount + "g";
 
-    const carbohydrate_amount = data["carbohydrate_by_difference"];
+    const carbohydrate_amount = target_food_data["carbohydrate_by_difference"];
     console.log(`Carbohydrate amount: ${carbohydrate_amount}`)
     document.getElementById("carbohydrate_amount").textContent = carbohydrate_amount + "g";
 
-    const fat_amount = data["total_lipid_fat"];
+    const fat_amount = target_food_data["total_lipid_fat"];
     console.log(`Fat amount: ${fat_amount}`)
     document.getElementById("fat_amount").textContent = fat_amount + "g";
 
